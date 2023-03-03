03630989e5ff0551ca49971197001776503c0f28
Samuel Hipólito <samuelhplt@gmail.com>
2023-03-03 17:51:10 -0300

    Setup inicial do projeto, adição da classe de contexto para acesso ao banco, criação da model para vídeos.

---
+++ AluraFlixChallenge.API/Context/MongoDbContext.cs
@@ -0,0 +1,38 @@
+﻿using AluraFlixChallenge.API.Entities;
+using MongoDB.Driver;
+
+
+
+namespace AluraFlixChallenge.API.Context
+{
+    public class MongoDbContext
+    {
+        public static string ConnectionString { get; set; }
+        public static string Database { get; set; }
+        private IMongoDatabase _mongoDatabase { get; }
+
+        public MongoDbContext()
+        {
+            try
+            {
+                MongoClientSettings settings = MongoClientSettings.FromConnectionString(ConnectionString);
+                MongoClient mongoClient = new(settings);
+
+                _mongoDatabase = mongoClient.GetDatabase(Database);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Couldn't connect to server: {ex.Message}");
+            }
+        }
+
+        public IMongoCollection<Video> Videos 
+        {
+            get
+            {
+                return _mongoDatabase.GetCollection<Video>("Video");
+            }
+        }
+
+    }
+}
---
+++ AluraFlixChallenge.API/Entities/Video.cs
@@ -0,0 +1,30 @@
+﻿namespace AluraFlixChallenge.API.Entities
+{
+    public class Video
+    {
+        public long Id { get; set; }
+
+        private string title;
+        private string description;
+        private string url;
+        
+
+        public string Title 
+        { 
+            get { return title; } 
+            set { title = value; } 
+        }
+
+        public string Description 
+        {
+            get { return description; } 
+            set { description = value; }
+        }
+
+        public string Url
+        {
+            get { return url; }
+            set { url = value; }
+        }
+    }
+}
