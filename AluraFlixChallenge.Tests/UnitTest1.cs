3299c65a86af9fe7b24d5f2b2af13fb314ee4c86
Samuel Hipólito <samuelhplt@gmail.com>
2023-03-13 14:55:51 -0300

    criação do projeto para o desenvolvimento dos testes

---
+++ AluraFlixChallenge.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AluraFlixChallenge.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ AluraFlixChallenge.Tests/VideoTests.cs
@@ -0,0 +1,18 @@
+﻿using AluraFlixChallenge.API.Entities;
+
+namespace AluraFlixChallenge.Tests
+{
+
+    public class VideoTests
+    {
+        private List<Video> video;
+
+
+
+        [Fact]
+        public void GetAllVideos()
+        {
+
+        }
+    }
+}
